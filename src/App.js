b4804949d8ebdc70cb03f0c4a5f30b2dd045a68c
Geraldd21 <dongpatalagan@gmail.com>
2026-09-25 18:17:47 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Footer from './Footer';
+import ContactForm from './ContactForm';
+function App() {
+ return (
+ <div>
+ <Navbar />
+ <Hero />
+  <ContactForm />
+ <Footer />
+ </div>
+ );
+}
+export default App;
